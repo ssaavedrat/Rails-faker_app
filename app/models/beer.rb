0ec681611345bd9bc2ce2e94f4bb75e1e6ef818c
Sergio Saavedra <sergio.saavedra@ug.uchile.cl>
2023-07-28 19:43:52 -0400

    update annotate models

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: beers
+#
+#  id              :bigint           not null, primary key
+#  brand           :string
+#  name            :string
+#  alcoholic_grade :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class Beer < ApplicationRecord
 end
--- test/models/beer_test.rb
+++ test/models/beer_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: beers
+#
+#  id              :bigint           not null, primary key
+#  brand           :string
+#  name            :string
+#  alcoholic_grade :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require "test_helper"
 
 class BeerTest < ActiveSupport::TestCase
